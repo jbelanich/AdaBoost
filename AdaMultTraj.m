dcbebcbfae6806071b6c911005bc6106980f35f8
josh <josh@josh-MacBookPro.(none)>
2013-03-25 13:34:53 -0400

    Modified something

--- AdaMultTraj.m
+++ AdaMultTraj.m
@@ -13,7 +13,7 @@ AllWsStart = AllWs;
 
 for i = 1:T
     disp(i);
-    
+    fflush(1)
     for j = 1:numPoints
         [AllWs(:,j), AllError(i,j), AllHyp(i,j),AllDistanceFromBound(i,j),AllDifferences(i,j)] = AdaUpdate(AllWs(:,j), M);
     end
